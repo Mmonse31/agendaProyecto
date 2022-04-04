977ed1a16f56f8887500fb4da3281eca9aec9f1e
Montserrat Montano <te-montanom@sony-ste.sel.sony.com>
2022-04-03 17:54:50 -0700

    cambios en equema y formulario

--- src/js/tasks-utils.js
+++ src/js/tasks-utils.js
@@ -1,19 +1,13 @@
 const TaskSchema = {
-    status: { 
-      label: "Status",
-      type: "string",
+    hora: {
+      label: "Hora",
+      type: "time",
       required: true,
-      values: ["pending", "deleted", "completed", "waiting", "recurring"],
     },
-    uuid: { 
-      label: "ID",
-      type: "uuid",
-      required: true
-    },
-    entry:  { 
-      label: "Entry Date",
+    date: {
+      label: "Date",
       type: "date",
-      required: true
+      required: true,
     },
     description: { 
       label: "Description",
@@ -21,69 +15,9 @@ const TaskSchema = {
       required: true,
       validation: (value) => values.length > 2
     },
-    start: { 
-      label: "Start Date",
-      type: "date"
-    },
-    end: { 
-      label: "End Date",
-      type: "date"
-    },
-    due: { 
-      label: "Due Date",
-      type: "date"
-    },
-    until: { 
-      type: "date"
-    },
-    wait: { 
-      type: "date"
-    },
-    modified: { 
-      label: "Modified At",
-      type: "date"
-    },
-    scheduled: { 
-      type: "date"
-    },
-    recur: { 
-      type: "string"
-    },
-    mask: { 
-      type: "string"
-    },
-    imask: { 
-      type: "integer"
-    },
-    parent: { 
-      label: "Parent Task",
-      type: "uuid"
-    },
-    project: { 
-      label: "Project",
-      type: "string"
-    },
-    priority: { 
-      label: "priority",
-      type: "string"
-    },
-    depends: { 
-      label: "Depends",
-      type: "string"
-    },
-    tags: { 
-      label: "Tags",
-      type: "array"
-    },
-    annotation: { 
-      label: "Annotations",
-      type: "array"
-    }
+   
 }
 
-function DurationParser(durationString) {
-    return ""
-}
 function uuidv4() {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
@@ -121,11 +55,6 @@ function TaskValidator(data) {
 
 function TaskCreate(data) {
   if (!data.uuid) { data.uuid = uuidv4(); }
-  if (!TaskSchema.status.includes(data.status)) { data.status = "pending" } 
-  if (!data.project) { data.project = [] }
-  if (!Array.isArray(data.project)) { data.project = [data.project] }
-  if (!data.tags) { data.tags = [] }
-  if (!Array.isArray(data.tags)) { data.tags = [data.tags] }
 
   console.table(data);
   return GetTasksDatabase().then(db => {
