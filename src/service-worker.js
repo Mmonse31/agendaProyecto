17041acde99ba0eb11ddfbcab16b329635172577
Montserrat Montano <te-montanom@sony-ste.sel.sony.com>
2022-04-03 22:13:13 -0700

    save cache storage

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,11 @@
 const SW_VERSION = "0.0.1"
 const CACHE_VERSION = "v1"
-const CACHE_RESOURCES = ["/apple-touch-icon.png"]
+const CACHE_RESOURCES = [
+  "/apple-touch-icon.png",
+  "/favicon-32x32.png",
+  "/favicon-16x16.png",
+  "/icofont/icofont.min.css",
+]
 
 self.addEventListener("install", (event) => {
   console.log(`Installing service worker ${SW_VERSION}`);
